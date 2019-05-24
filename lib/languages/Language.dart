99a9fd38d0abd70023cc7146a1585a161c3ab7d9
otboss <otsurfer6@gmail.com>
2019-05-23 21:25:23 -0500

    Added FutureBuilder method to language class

--- lib/languages/Language.dart
+++ lib/languages/Language.dart
@@ -16,6 +16,27 @@ class Language{
   });
 
   String defaultLanguage = SupportedLanguages.en_US.toString();
+
+  FutureBuilder<String> getTranslation(BuildContext context, LanguageClassAttributes attribute){
+    return FutureBuilder<String>(
+      future: _getLanguageStringFromFile(context, attribute) ,
+      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return Text('');
+          case ConnectionState.active:
+            return Text('');
+          case ConnectionState.waiting:
+            return Text('');
+          case ConnectionState.done:
+            if (snapshot.hasError)
+              return Text('Error: ${snapshot.error}');
+            return Text('${snapshot.data}');
+        }
+        return null; // unreachable
+      },
+    );     
+  }
 }
 
 //For each UI text variable defined above enter its variable 
@@ -25,15 +46,15 @@ enum LanguageClassAttributes{
   chatScreenTitle
 }
 
-Locale getLocality(BuildContext context){
+Locale _getLocality(BuildContext context){
   return Localizations.localeOf(context);
 }
 
-String getSystemLanguage(BuildContext context){
+String _getSystemLanguage(BuildContext context){
   return Localizations.localeOf(context).toString();
 }
 
-Future<String> getLanguageFile(String languageFileName) async {
+Future<String> _getLanguageFile(String languageFileName) async {
   try {
     String contents = await rootBundle
         .loadString('assets/languages/$languageFileName.json');
@@ -43,25 +64,32 @@ Future<String> getLanguageFile(String languageFileName) async {
   }
 }
 
-Language generateLanguageClass(Map<String, String> languageFile){
+Language _generateLanguageClass(Map<String, String> languageFile){
   return new Language(
     homeScreenTitle: languageFile[LanguageClassAttributes.homeScreenTitle.toString()],
     chatScreenTitle: languageFile[LanguageClassAttributes.chatScreenTitle.toString()]
   );
 }
 
-Future<Language> generateFullLanguageClass(BuildContext context) async{
+Future<Language> _generateFullLanguageClass(BuildContext context) async{
   try{
-    String systemLanguage = getSystemLanguage(context);
-    Map<String, String> languageFile = json.decode(await getLanguageFile(systemLanguage));
-    return generateLanguageClass(languageFile);
+    String systemLanguage = _getSystemLanguage(context);
+    Map<String, String> languageFile = json.decode(await _getLanguageFile(systemLanguage));
+    return _generateLanguageClass(languageFile);
   }
   catch(err){
     return defaultLanguage;    
   }
 }
 
-enum SupportedLanguages{
-  en_US,
-  zh_CN
+Future<String> _getLanguageStringFromFile(BuildContext context, LanguageClassAttributes attribute) async{
+  try{
+    String systemLanguage = _getSystemLanguage(context);
+    return json.decode(await _getLanguageFile(systemLanguage))[attribute];
+  }
+  catch(err){
+    print(err);
+    return null;    
+  }
 }
+
