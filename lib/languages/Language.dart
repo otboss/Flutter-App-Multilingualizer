30698735d2582a152d8cd47792e7ca4d4e773a59
otboss <otsurfer6@gmail.com>
2019-05-24 00:16:44 -0500

    minor changes

--- lib/languages/Language.dart
+++ lib/languages/Language.dart
@@ -2,24 +2,41 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import './DefaultLanguage.dart';
+///Update this method for each new Language class attribute
+enum LanguageClassAttributes { homeScreenTitle, chatScreenTitle }
 
-class Language{
-  //Define all UI text variables that your wish to translate
-  //between the delimiters below.
-  //&& <- START DELIMITER
+class Language {
+  //Define all new UI Text to be translated here and update the toJSON 
+  //method and LanguageClassAttributes enum
   final String homeScreenTitle;
   final String chatScreenTitle;
-  //&& <- END DELIMITER  
+
+  ///Converts the Language class to a Map. Update this method for each 
+  ///new Language class attribute
+  Map toJSON() {
+    return {
+      "homeScreenTitle": this.homeScreenTitle,
+      "chatScreenTitle": this.chatScreenTitle
+    };
+  }
+
   Language({
-    @required this.homeScreenTitle,
+    @required this.homeScreenTitle, 
     @required this.chatScreenTitle
   });
 
   String defaultLanguage = SupportedLanguages.en.toString();
 
-  FutureBuilder<String> getTranslation(BuildContext context, LanguageClassAttributes attribute){
+  ///Takes three mandatory parameters. The first two are the BuildContext
+  /// and the desired Language class attribute. The Language parameter 
+  /// is the result of the asynchronous creation of a Language object 
+  /// (requires reading a json asset file). If the languageLibrary object 
+  /// is null then it has not yet loaded. This parameter will enable the
+  ///app to read from memory only after the Language object has been created 
+  ///for better performance.
+  FutureBuilder<String> getTranslation(BuildContext context, LanguageClassAttributes attribute, Language languageLib) {
     return FutureBuilder<String>(
-      future: _getLanguageStringFromFile(context, attribute) ,
+      future: _getLanguageStringFromFile(context, attribute, languageLib),
       builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.none:
@@ -29,24 +46,15 @@ class Language{
           case ConnectionState.waiting:
             return Text('');
           case ConnectionState.done:
-            if (snapshot.hasError)
-              return Text('Error: ${snapshot.error}');
+            if (snapshot.hasError) return Text('Error: ${snapshot.error}');
             return Text('${snapshot.data}');
         }
-        return null; // unreachable
       },
-    );     
+    );
   }
 }
 
-//For each UI text variable defined above enter its variable 
-//name in the enum below
-enum LanguageClassAttributes{
-  homeScreenTitle, 
-  chatScreenTitle
-}
-
-enum SupportedLanguages{
+enum SupportedLanguages {
   al,
   bg,
   cs,
@@ -72,51 +80,51 @@ enum SupportedLanguages{
   zh_TW
 }
 
-Locale _getLocality(BuildContext context){
+Locale _getLocality(BuildContext context) {
   return Localizations.localeOf(context);
 }
 
-String _getSystemLanguage(BuildContext context){
-  if(Localizations.localeOf(context).toString() == "zh_CN" || Localizations.localeOf(context).toString() == "zh_TW")
-    return Localizations.localeOf(context).toString(); 
+String _getSystemLanguage(BuildContext context) {
+  if (Localizations.localeOf(context).toString() == "zh_CN" || Localizations.localeOf(context).toString() == "zh_TW")
+    return Localizations.localeOf(context).toString();
   return Localizations.localeOf(context).toString().substring(0, 2);
 }
 
 Future<String> _getLanguageFile(String languageFileName) async {
   try {
-    String contents = await rootBundle
-        .loadString('assets/languages/$languageFileName.json');
+    String contents = await rootBundle.loadString('assets/languages/$languageFileName.json');
     return contents;
   } catch (err) {
     return null;
   }
 }
 
-Language _generateLanguageClass(Map<String, String> languageFile){
+Language _generateLanguageClass(Map<String, String> languageFile) {
   return new Language(
-    homeScreenTitle: languageFile[LanguageClassAttributes.homeScreenTitle.toString()],
-    chatScreenTitle: languageFile[LanguageClassAttributes.chatScreenTitle.toString()]
-  );
+      homeScreenTitle: languageFile[LanguageClassAttributes.homeScreenTitle.toString()],
+      chatScreenTitle: languageFile[LanguageClassAttributes.chatScreenTitle.toString()]);
 }
 
-Future<Language> _generateFullLanguageClass(BuildContext context) async{
-  try{
+Future<Language> _generateFullLanguageClass(BuildContext context) async {
+  try {
     String systemLanguage = _getSystemLanguage(context);
     Map<String, String> languageFile = json.decode(await _getLanguageFile(systemLanguage));
     return _generateLanguageClass(languageFile);
-  }
-  catch(err){
-    return defaultLanguage;    
+  } catch (err) {
+    return defaultLanguage;
   }
 }
 
-Future<String> _getLanguageStringFromFile(BuildContext context, LanguageClassAttributes attribute) async{
-  try{
+Future<String> _getLanguageStringFromFile(BuildContext context, LanguageClassAttributes attribute, Language languageLibrary) async {
+  try {
+    if (languageLibrary != null) {
+      if (languageLibrary.toJSON()[attribute] != null)
+        return languageLibrary.toJSON()[attribute];
+    }
     String systemLanguage = _getSystemLanguage(context);
     return json.decode(await _getLanguageFile(systemLanguage))[attribute];
-  }
-  catch(err){
+  } catch (err) {
     print(err);
-    return null;    
+    return null;
   }
 }
